1e827ec40d2b0b683aae8295482d0162c12ea851
ubuntu <68152822+cmark0v@users.noreply.github.com>
2023-11-22 17:35:56 +0900

    add special chars

---
+++ tests/test_uqspecial.py
@@ -0,0 +1,24 @@
+import unittest
+import jsonofabitch as jsob
+import json
+
+special = ["_", "-", "~", "$", "@", "&", "?", ".", "%"]
+datajsob = []
+datacheck = []
+for s in special:
+    datajsob.append(
+        f"b{s}ob:g=5, h{s}=4 ; {s}b=(6,6,3,), f=string{s}, l=[3,3,3,3,]",
+    )
+    datacheck.append(
+        {f"b{s}ob": {"g": 5, f"h{s}": 4, f"{s}b": (6, 6, 3), "f": f"string{s}", "l": [3, 3, 3, 3]}}
+    )
+
+
+class TestJSOB(unittest.TestCase):
+    def test_uqspecial(self):
+        for j, d in enumerate(datajsob):
+            self.assertEqual(jsob.loads(d), datacheck[j])
+
+
+if __name__ == "__main__":
+    unittest.main()
