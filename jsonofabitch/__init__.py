8c5bcdcd5c669988392130423611102068ad1d82
ubuntu <68152822+cmark0v@users.noreply.github.com>
2023-12-04 00:24:37 +0900

    merge v0.4

--- jsonofabitch/__init__.py
+++ jsonofabitch/__init__.py
@@ -1,5 +1,5 @@
 """
-JSONofabitch
+## JSONofabitch
 
 functions
 ---------
@@ -13,24 +13,29 @@ JSLOB syntax
 ------------
 
 - keypairs -  ``:`` or ``=`` separate key value pairs, whitespace is ignored  ``'g=5' -> {"g": 5}`` and ``'g:5' -> {"g": 5}``, such keypairs are implicitly assumed to be in a JSLOB object which resolves to a python dict
-- keys - double quoted string or alphanumeric plus underscore, non-numeric first character(same as python vars). 
+- keys - double quoted string or alphanumeric plus ``_-~$@&?.%<>#!^+/*``(no spaces,no unicode)  non-numeric first character. 
 - values - another JSLOB object, a tupple, a list, string without quotes, quote-enclosed string, boolean ``true`` ``false`` , ``null`` , integers, floats
 - ``'true' -> True`` ``'false' -> False`` ``'null' -> None`` case sensitive
 - tuples in ``( )`` lists in ``[ ]`` objects separated by comma ``,`` or semicolon ``;`` . trailing comma/semicolon okay
 - dicts/JSLOB objects have keypairs separated by semicolon or comma, nested jslob objects(in lists, tuples or jslob objects) are terminated and separated simultaneously by ``;;`` or ``,,`` , IE: ``'[g=5,h=3]' -> [{'g': 5, 'h': 3}]`` and ``'[g=5;;h=3]' -> [{'g': 5}, {'h': 3}]``
-- strings - unquoted strings must be alphanumeric plus ``_-~$@&?.%<>#!^+/*``(no spaces,no unicode)  non-numeric first character. only double quotes can be used in the case of quoted strings, which do not have any restriction other than the escape of double quotes
+- strings - same as keys, mostly only need quotes for numeric first character or if it includes spaces.
 - scientific notation ``2e4`` resolves to $2.0 * 10^4$, always resolve to float
 - numbers with decimal present resolve to floats while others resolve to ints IE ``'3.0' -> 3.0`` and ``'3' -> 3`` ``'3.' -> 3.0``
 
 examples
 --------
 
+```python
 import jsonofabitch as jsob
-jsob.loads("f=5, size=big, style=SLOB, coord=[(3,4),(1.2,3)]")
+d = jsob.loads("f=5, size=big, style=SLOB, coord=[(3,4),(1.2,3)]")
 #{'f': 5, 'size': 'big', 'style': 'SLOB', 'coord': [(3, 4), (1.2, 3)]}
 
 jsob.dumps(d)
 #Out[3]: '{"f": 5, "size": "big", "style": "SLOB", "coord": [[3, 4], [1.2, 3]]}'
+
+jsob.dumpslob(d)
+#Out[4]: ' "f"=  5;  "size"="big";style=  "SLOB";  coord: [ (3 ,4 ), (1.2, 3,)  ];'
+```
 """
 
 __version__: str = "0.4.0"
